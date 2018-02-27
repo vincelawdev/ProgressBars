c60425c933f813905032dd4c8a7b995e7e153e39
Vince Law <vince@siriusdesignlab.com.au>
2018-02-27 22:29:56 +1100

    App component successfully populated the state with the data from the API endpoint.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          Test
-      </div>
-    );
-  }
+    state = {
+        buttons: [],
+        bars: []
+    };
+
+    componentDidMount() {
+        axios.get('https://frontend-exercise.apps.b.cld.gov.au/bars')
+            .then(response => {
+                this.setState({ buttons: response.data.buttons, bars: response.data.bars });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.bars.map(bar => (<p key={bar.toString()}>Bar: {bar}</p>))}
+                {this.state.buttons.map(button => (<p key={button.toString()}>Button: {button}</p>))}
+            </div>
+        );
+    }
 }
 
 export default App;
